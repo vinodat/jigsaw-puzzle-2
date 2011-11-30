21eca7e4de237f0c4f1a42663304869110f9c4fd
yuqianguo <yuqianguo@gmail.com>
2011-11-30 07:34:43 +0000

    new update .add move logic.still not finish. need to add animation.

--- jigdrawpuzzle/src/com/yqg/puzzle/JigdrawPuzzleMain.java
+++ jigdrawpuzzle/src/com/yqg/puzzle/JigdrawPuzzleMain.java
@@ -1,27 +1,19 @@
 package com.yqg.puzzle;
 
-import com.yqg.puzzle.view.TileView;
-
 import android.app.Activity;
-import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
-import android.graphics.drawable.ShapeDrawable;
-import android.graphics.drawable.TransitionDrawable;
 import android.os.Bundle;
 import android.view.Display;
-import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.view.ViewGroup.LayoutParams;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.ImageView.ScaleType;
+
+import com.yqg.puzzle.view.TileView;
 
 public class JigdrawPuzzleMain extends Activity {
 	
-	private Bitmap[][] bmps = new Bitmap[3][3];
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -29,28 +21,21 @@ public class JigdrawPuzzleMain extends Activity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
-       /* setContentView(R.layout.main);
-        Resources res = getResources();
-        TransitionDrawable transition = (TransitionDrawable) res.getDrawable(R.drawable.transtion);
-        ImageView image = new ImageView(this);
-        image.setImageDrawable(transition);
-        transition.startTransition(1000);
-        LinearLayout layout = (LinearLayout) findViewById(R.id.imageslayout);
-        layout.addView(image);*/
         
         Display display = getWindowManager().getDefaultDisplay(); 
         int dwidth = display.getWidth();
-        int dheight = display.getHeight();
+        int dheight = display.getHeight() - 80;
         
         Drawable dbmp = getResources().getDrawable(R.drawable.test);
         Bitmap bitmap = ((BitmapDrawable)dbmp).getBitmap();
         
         TileView tv = new TileView(this);
-        tv.init(2, dwidth, dheight, bitmap);
-        
-        setContentView(tv);
-        
+        if(!tv.init(1, dwidth, dheight, bitmap)){//if init fail.go to choose panel.
+        	//TODO process
+        }else{//init suc,show
+        	setContentView(R.layout.puzzleview);
+            LinearLayout puzzleLayout = (LinearLayout) findViewById(R.id.puzzle_view);
+            puzzleLayout.addView(tv);
+        }
     }
-    
-    
 }
--- jigdrawpuzzle/src/com/yqg/puzzle/view/TileModel.java
+++ jigdrawpuzzle/src/com/yqg/puzzle/view/TileModel.java
@@ -1,48 +1,64 @@
 package com.yqg.puzzle.view;
 
+import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.view.View;
 /**
  * <p>
- * represent the Tile.which is the basic unit of the game.
+ * sigle image tile infomation.
  * </p>
  * @author Administrator
  *
  */
-public class TileModel {
-
-	private int mXPixel = 0;
-	private int mYPixel = 0;
-	private Bitmap mTileImage = null;
-	private int mWidth = 0;
-	private int mHeight = 0;
-	public int getmWidth() {
-		return mWidth;
+public class TileModel extends View{
+	public TileModel(Context context) {
+		super(context);
 	}
-	public void setmWidth(int mWidth) {
-		this.mWidth = mWidth;
+	private int mXIndex = -1,mYIndex = -1;
+	private int mBitmapIndex = -1;
+	private Bitmap mBitmap = null;
+	private Rect mAreaRect = new Rect(0,0,0,0);
+	
+	public Bitmap getmBitmap() {
+		return mBitmap;
 	}
-	public int getmHeight() {
-		return mHeight;
+	public void setmBitmap(Bitmap mBitmap) {
+		this.mBitmap = mBitmap;
 	}
-	public void setmHeight(int mHeight) {
-		this.mHeight = mHeight;
+	
+	public int getmXIndex() {
+		return mXIndex;
 	}
-	public int getmXPixel() {
-		return mXPixel;
+	public void setmXIndex(int mXIndex) {
+		this.mXIndex = mXIndex;
 	}
-	public void setmXPixel(int mXPixel) {
-		this.mXPixel = mXPixel;
+	public int getmYIndex() {
+		return mYIndex;
 	}
-	public int getmYPixel() {
-		return mYPixel;
+	public void setmYIndex(int mYIndex) {
+		this.mYIndex = mYIndex;
 	}
-	public void setmYPixel(int mYPixel) {
-		this.mYPixel = mYPixel;
+	public int getmBitmapIndex() {
+		return mBitmapIndex;
 	}
-	public Bitmap getmTileImage() {
-		return mTileImage;
+	public void setmBitmapIndex(int mBitmapIndex) {
+		this.mBitmapIndex = mBitmapIndex;
 	}
-	public void setmTileImage(Bitmap mTileImage) {
-		this.mTileImage = mTileImage;
+	public Rect getmAreaRect() {
+		return mAreaRect;
+	}
+	public void setmAreaRect(Rect mAreaRect) {
+		this.mAreaRect = mAreaRect;
+	}
+	
+	@Override
+	protected void onDraw(Canvas canvas) {
+		super.onDraw(canvas);
+		canvas.drawBitmap(
+				mBitmap, null,
+				getmAreaRect(), new Paint());
 	}
 }

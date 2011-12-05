e9e436215311f6a98794be460f07ffd6bcc675e5
yuqianguo@gmail.com <yuqianguo@gmail.com@70ec8b1f-24a9-7dc2-42d4-39811e8273d6>
2011-12-05 05:38:27 +0000

    add preferences setting. not finish.

---
+++ trunk/jigdrawpuzzle/src/com/yqg/puzzle/PreferSettingActivity.java
@@ -0,0 +1,12 @@
+package com.yqg.puzzle;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class PreferSettingActivity extends PreferenceActivity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+}
